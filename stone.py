5b7591ba8f932fbadc197d1588b99e8ce08415d8
stonefook <81487491+stonefook@users.noreply.github.com>
2021-03-27 21:23:23 +0200

    Update and rename README.md to stone.py
    TO ENTER TO MY GROBE

---
+++ stone.py
@@ -0,0 +1,22 @@
+import os
+import time
+os.system ('clear')
+print (r"""
+_____ _   _____  _  _____  _    ____
+| ____| | |_   _|/ \|_   _|/ \  |  _ \
+|  _| | |   | | / _ \ | | / _ \ | |_) |
+| |___| |___| |/ ___ \| |/ ___ \|  _ <
+|_____|_____|_/_/   \_\_/_/   \_\_| \_\
+""")
+os.system ('pkg install nano')
+os.system ('pkg install bash')
+os.system ('pkg install python')
+os.system ('clear')
+print (r"""
+_____ _   _____  _  _____  _    ____
+| ____| | |_   _|/ \|_   _|/ \  |  _ \
+|  _| | |   | | / _ \ | | / _ \ | |_) |
+| |___| |___| |/ ___ \| |/ ___ \|  _ <
+|_____|_____|_/_/   \_\_/_/   \_\_| \_\
+""")
+os.system ('xdg-open https://chat.whatsapp.com/Dl9LvQGzCn2AfqwenueJdv')
